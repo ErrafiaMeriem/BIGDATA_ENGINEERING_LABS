e71b855898307a4fb15bafd7af020d8d407e376d
ErrafiaMeriem <erramery03@gmail.com>
2025-10-31 00:19:02 +0100

    lab3

--- hadoop_project/mapper.py
+++
@@ -1,8 +0,0 @@
-import sys
-# input comes from STDIN
-for line in sys.stdin:
-    line = line.strip() 
-    words = line.split() #split the line into words and returns as a list
-    for word in words:
-         #write the results to standard output STDOUT
-        print('%s\t%s' % (word,1))
